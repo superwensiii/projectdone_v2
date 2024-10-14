e01a222ec09c389da5b2cda515b2cead233a7239
czeddy <srymeczeddrick@gmail.com>
2024-10-14 22:14:42 +0800

    12th commit

--- resetpass.php
+++ resetpass.php
@@ -10,34 +10,50 @@
     <title>Password Reset - SB Admin</title>
     <link href="../css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>|
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Password Reset - SB Admin</title>
+    <link href="../css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 
-<body class="bg-dark">
-    <div id="layoutAuthentication">
+<body class="bg-dark d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
+    <div id="layoutAuthentication" class="w-100">
         <div id="layoutAuthentication_content">
             <main>
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-5">
-                            <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4">Password Recovery</h3>
+                            <div class="card border-0 rounded-lg mt-5 shadow">
+                                <div class="card-header bg-primary text-white text-center">
+                                    <h3 class="font-weight-light my-4">Password Recovery</h3>
                                 </div>
                                 <div class="card-body">
-                                    <div class="small mb-3 text-muted">Enter your email address and we will send you a
-                                        link to reset your password.</div>
+                                    <div class="small mb-3 text-muted text-center">Enter your email address and we will send you a link to reset your password.</div>
                                     <form method="POST" action="mailer.php">
                                         <div class="form-floating mb-3">
                                             <input class="form-control" id="inputEmail" name="email" type="email" placeholder="name@example.com" required />
                                             <label for="inputEmail">Email address</label>
                                         </div>
-                                        <div class="d-flex align-items-center justify-content-end mt-4 mb-0">
-                                            <button type="submit" class="btn btn-primary align-items-center">Reset Password</button>
+                                        <div class="d-flex justify-content-center mt-4 mb-0">
+                                            <button type="submit" class="btn btn-primary">Reset Password</button>
                                         </div>
                                     </form>
                                 </div>
                                 <div class="card-footer text-center py-3">
-                                    <div class="big"><a class="small" href="../main/login.php">Return to login</a></div>
+                                    <div><a class="small" href="user_login.php">Return to login</a></div>
                                 </div>
                             </div>
                         </div>
@@ -45,23 +61,8 @@
                 </div>
             </main>
         </div>
-        <div id="layoutAuthentication_footer">
-            <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid px-4">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        crossorigin="anonymous"></script>
+        
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="/js/scripts.js"></script>
 </body>
 
